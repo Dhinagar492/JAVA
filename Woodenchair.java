f96e91fc0ab9eef5b0d879daa04c41fb899c3527
Dhinager492 <dhinagar294@gmail.com>
2022-10-20 09:28:32 +0530

    first

---
+++ Woodenchair.java
@@ -0,0 +1,9 @@
+class Woodenchair{
+String colour="yellow"; 
+private void Seating(){
+System.out.println("wood");
+//public static void main(String[]args){
+//Woodenchair w=new Woodenchair();
+//System.out.println(w.colour);
+}
+}//method overriding with Cusionchair.java
