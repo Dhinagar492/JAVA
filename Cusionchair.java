abde827c98a852d698eca279a2dd801bf939ca7b
Dhinagar492 <115131623+Dhinagar492@users.noreply.github.com>
2022-12-21 21:52:35 +0530

    Add files via upload

---
+++ Cusionchair.java
@@ -0,0 +1,15 @@
+class Cusionchair extends Woodenchair{
+String colour="black";
+//int colour=5;
+
+public void Seating(){
+System.out.println("sponge");
+}
+
+public static void main(String[]args){
+Cusionchair c=new Cusionchair();
+System.out.println(c.colour);
+c.Seating();
+}
+}
+//method overriding with Woodenchair.java
